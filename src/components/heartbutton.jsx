005938b4cb7330f6a5cd4b084187db05cdf5c296
Kellories <thaisiang.22@ichat.sp.edu.sg>
2022-12-08 17:58:54 +0800

    small styling edits to like button

--- src/components/heartbutton.jsx
+++ src/components/heartbutton.jsx
@@ -19,12 +19,8 @@ const LikeButton = () => {
         name="cards-heart-outline"
         size = {25}
         backgroundColor = "#f5f5f5"
-        iconStyle={{marginRight: 2}}
+        iconStyle={{marginRight: 0}}
         borderRadius = {100}
-
-    
-   
- 
         color={ notLiked ? 'red' : 'black'}
          >
         </MaterialCommunityIcons.Button>
