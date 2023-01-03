8d6bc260a1a9f8226fbd8f92a8569f2661915f29
avicity7 <74177090+avicity7@users.noreply.github.com>
2023-01-03 14:53:41 +0800

    feat: Article page

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,10 +1,31 @@
 import { Pressable, StyleSheet, Text } from "react-native";
 
-const Button = () => {
+const Button = ({text,onPress,customStyle}) => {
     return(
-        <Pressable>
-           <Text>Hi</Text> 
+        <Pressable
+            style = {style.container}
+            onPress = {onPress}
+        >
+           <Text style = {style.text}>{text}</Text> 
         </Pressable>
     )
 }
+
+const style = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        padding: 16,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        borderTopColor: "#F0F0F0",
+        borderTopWidth: 1,
+        borderBottomColor: "#F0F0F0",
+        borderBottomWidth: 1
+    },
+    text: { 
+        fontSize: 16
+    }
+  });
+  
 export default Button;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,75 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View, ScrollView } from 'react-native';
 import React, {useState, useEffect} from 'react';
 import globalStyles from "../styles/global";
 import WriteButton from '../components/writebutton';
-const Home = () => {
+import Button from '../components/button';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const ArticlesView = ({ navigation }) => {
     return ( 
         <View style={globalStyles.container}>
-        
-            <WriteButton/>
-
             <Text
                 style={globalStyles.header}
             >
                 Articles
             </Text>
+            <ScrollView>
+
+                <Button 
+                    style = {globalStyles.articleCard}
+                    text = {"Open Article"}
+                    onPress = {()=>{navigation.navigate("Article")}}
+                />
+            
+            </ScrollView>
             
         </View>
     )
 }
+
+const Article = ({route}) => { 
+
+    return (
+        <View style={globalStyles.container}>
+            <ScrollView>
+                <Text
+                    style={globalStyles.header}
+                >
+                    Article
+                </Text>
+            </ScrollView>
+            
+        </View>
+    )
+}
+
+const Stack = createNativeStackNavigator();
+
+const Home = () => {
+    return (
+      <NavigationContainer independent={true}>
+        <Stack.Navigator initialRouteName="Articles">
+          <Stack.Screen
+            name="Articles"
+            component={ArticlesView}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Article"
+            component={Article}
+            options={{
+              headerBackTitle: "",
+              headerTintColor: "black",
+              headerTitleStyle: {
+                color: "black",
+              }
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  };
 
 export default Home 
