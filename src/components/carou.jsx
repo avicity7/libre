4879312b58d2a6c71c631c205ad74dbf1270eec0
Thai Siang <thaisiang.22@ichat.sp.edu.sg>
2023-01-27 10:10:44 +0800

    fix: Migrate database to json

--- src/components/carou.jsx
+++ src/components/carou.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {Text, View, SafeAreaView, Image, ImageBackground,Dimensions} from 'react-native';
 
 import Carousel, {Pagination}from '../react-native-snap-carousel';
+const database = require('../../api/database.json')
 
 export default class ArticleCarou extends React.Component {
 
@@ -10,23 +11,8 @@ export default class ArticleCarou extends React.Component {
         super(props);
         this.state = {
           activeIndex:0,
-          carouselItems: [
-          {
-              title:"The Amazon Fire",
-              text: "What really happened in the fight against the Fire.",
-              image: 'https://static.scientificamerican.com/sciam/cache/file/C1CBDCD7-0471-4C47-846607ADAF8585F4_source.jpg'
-          },
-          {
-              title:"A Hustler's View on Life",
-              text: "What life means to someone who is striving for success",
-              image: 'https://images.unsplash.com/photo-1671725501835-afb7bd1f21ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80'
-          },
-          {
-              title:"China's Lack Of Work Life Balance",
-              text: "And How today's generation is responding.",
-              image: "https://images.unsplash.com/photo-1581252165015-9b5151a6930e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=766&q=80"
-          },
-        ]
+          carouselItems: database.carouselItems,
+        
       }
     }
 
